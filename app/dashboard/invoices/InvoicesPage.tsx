c3febc0c4a8e8e43863646d27bb8a84920e07821
JSSatorres <juansataz.dev@gmail.com>
2024-09-05 22:14:44 +0200

    feat: remove modify models invoice

--- app/dashboard/invoices/InvoicesPage.tsx
+++ app/dashboard/invoices/InvoicesPage.tsx
@@ -12,7 +12,6 @@ export default async function InvoicesPage({
   searchParams?: {
     query?: string;
     page?: string;
-    isEditing?: string;
   };
 }) {
   const query = searchParams?.query || "";
